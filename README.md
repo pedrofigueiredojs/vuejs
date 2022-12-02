82ef20dc9afa076e4afa8a8923593082693cb58c
Pedro Figueiredo <pedrofigueiredojs@gmail.com>
2022-12-02 20:03:04 -0300

    feat: add new vuejs project

---
+++ README.md
@@ -0,0 +1,6 @@
+# Alura Email Marketing Responsivo
+
+## Links para testes
+
+* [Vercel](https://alura-email-marketing-responsivo.vercel.app/)
+* [Github Pages](https://pedrofigueiredojs.github.io/alura-email-marketing-responsivo/)
---
+++ src/components/Comments.vue
@@ -0,0 +1,63 @@
+<template>
+  <div class="container">
+    <h1>Comentários</h1>
+    <hr />
+    <FormTodo v-on:add-todo="addComment" />
+    <div class="list-group mt-4">
+      <p v-if="comments.length <= 0">Sem comentários...</p>
+      <div
+        class="list-group-item"
+        v-for="(comment, index) in allComments"
+        v-bind:key="index"
+        v-bind:index="index"
+      >
+        <span class="comment_author"
+          >Author: <strong>{{ comment.name }}</strong></span
+        >
+        <p>{{ comment.message }}</p>
+        <div>
+          <a href="#" title="Excluir" v-on:click.prevent="removeComment(index)"
+            >Excluir</a
+          >
+        </div>
+      </div>
+    </div>
+    <hr />
+  </div>
+</template>
+
+<script>
+import FormTodo from "./FormTodo";
+
+export default {
+  components: {
+    FormTodo,
+  },
+  data() {
+    return {
+      comments: [],
+    };
+  },
+  methods: {
+    addComment(comment) {
+      this.comments.push(comment);
+    },
+    removeComment(index) {
+      this.comments.splice(index, 1);
+    },
+  },
+  computed: {
+    allComments() {
+      return this.comments.map((comment) => ({
+        ...comment,
+        name: comment.name.trim() === "" ? "Anonimo" : comment.name,
+      }));
+    },
+  },
+  watch: {
+    comments(val) {
+      console.log("val", val);
+    },
+  },
+};
+</script>
